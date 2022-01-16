d70e2bfb90f88a58f2a7c57bce59395d57ecd835
eonil <pub.dev@eonil.com>
2022-01-16 22:51:06 +0900

    refactor: make ridl_tests

---
+++ ridl_tests/tests/images/input/rust1.rs
@@ -0,0 +1,54 @@
+//! Here be dragons.
+
+type Tuna = String;
+type Salmon = String;
+
+/// Ingredients of magic.
+#[serde(Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+enum Mineral {
+    IronMetal,
+    Alumina,
+    Arcana,
+}
+
+#[serde(Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+#[rest(in)]
+struct Pet {
+    name: Vec<String>,
+    /// Did they take a walk today?
+    #[query]
+    walk: bool,
+    #[path]
+    living_address: Option<Address>,
+    #[body]
+    #[mime("application/octet-stream")]
+    contents: Vec<u8>,
+}
+
+#[serde(Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+struct Address {
+    city: String,
+}
+
+/// Edibles.
+#[serde(Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+#[rest(out)]
+enum Dish {
+    #[status(200)]
+    #[mime("application/json")]
+    Sushi(Tuna),
+    /// Good salmons are reddish.
+    #[status(401)]
+    #[mime("application/json")]
+    PanFriedSteak(Salmon),
+}
+
+#[serde(Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+struct APIError {
+    message: String,
+}
---
+++ ridl_tests/tmp/typescript4/index.ts
@@ -0,0 +1,27 @@
+/// Here be dragons.
+type Tuna = string
+
+/// Ingredients of magic.
+enum Mineral {
+    ironMetal = "ironMetal",
+    alumina = "alumina",
+    arcana = "arcana"  
+}
+
+type Pet = {
+    name: string[]
+    /// Did they take a walk today?
+    walk: boolean
+    livingAddress?: Address
+}
+
+type Address = {
+    city: string
+}
+
+/// Edibles.
+type Dish = { sushi: Tuna } | { panFriedSteak: Salmon }
+
+type APIError = {
+    message: string
+}
